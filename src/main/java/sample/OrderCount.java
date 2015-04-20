f3099e6f42949139089fcb66df8f087dfbb18491
Cookie <cookie@matike.com.tw>
2015-04-20 16:55:04 +0800

    step 4

--- src/main/java/sample/OrderCount.java
+++ src/main/java/sample/OrderCount.java
@@ -2,7 +2,6 @@ package sample;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -26,17 +25,9 @@ import java.util.regex.Pattern;
  */
 public class OrderCount extends Configured implements Tool {
 
-    private Path inputPath;
-    private Path outputPath;
-
-    private final static Pattern pattern = Pattern.compile("([0-9]+),([0-9]+),([0-9]+)");
-
-    public OrderCount(Path inputPath, Path outputPath){
-        this.inputPath = inputPath;
-        this.outputPath = outputPath;
-    }
-
     public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+        private final static Pattern pattern = Pattern.compile("([0-9]+),([0-9]+),([0-9]+)");
 
         private Text word = new Text();
         private IntWritable price = new IntWritable();
@@ -63,8 +54,8 @@ public class OrderCount extends Configured implements Tool {
 
         @Override
         protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-
             int sum = 0;
+
             for (IntWritable value:values) {
                 sum += value.get();
             }
@@ -74,20 +65,17 @@ public class OrderCount extends Configured implements Tool {
 
     }
 
-    public int run(String[] strings) throws Exception {
+    public int run(String[] args) throws Exception {
 
         Configuration conf = new Configuration();
-
-        FileSystem fs = FileSystem.get(conf);
-        fs.delete(outputPath, true);
 
         Job job = Job.getInstance(conf);
 
         job.setJarByClass(OrderCount.class);
         job.setJobName("OrderCount");
 
-        FileInputFormat.setInputPaths(job, inputPath);
-        FileOutputFormat.setOutputPath(job, outputPath);
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
         // input
         job.setInputFormatClass(TextInputFormat.class);
@@ -115,10 +103,7 @@ public class OrderCount extends Configured implements Tool {
 
     public static void main(String[] args) {
         try{
-            Path inputPath = new Path(args[0]);
-            Path outputPath = new Path(args[1]);
-
-            ToolRunner.run(new OrderCount(inputPath, outputPath), args);
+            ToolRunner.run(new OrderCount(), args);
         }catch(Exception e){
             System.out.println("Usage: " + OrderCount.class.getSimpleName() + " [input] [output]");
         }
